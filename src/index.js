9461b42f5802d391479d0d47d2fd15e8c6b6dfa6
rorvts <rorvts@gmail.com>
2020-07-08 20:03:31 +0200

    Using React function approach

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
+
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button
+//         className="square"
+//         onClick={() => this.props.onClick()}
+//       >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
 
 class Board extends React.Component {
   constructor(props) {
@@ -23,10 +33,10 @@ class Board extends React.Component {
     };
   }
 
-  handleClick(i){
+  handleClick(i) {
     const squares = this.state.squares.slice();
     squares[i] = "X";
-    this.setState({squares: squares});
+    this.setState({ squares: squares });
   }
 
   renderSquare(i) {
